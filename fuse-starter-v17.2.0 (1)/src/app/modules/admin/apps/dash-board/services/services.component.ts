e3e8f48fea8f5b9b456ccf6e32398576311f3bb1
uncleyellowdk@ratraco.vn <uncleyellowdk@ratraco.vn>
2025-03-07 15:03:14 +0700

    sửa footer xong sửa ảnh động các tuyến đường xong sửa kích thước các ảnh xong

--- fuse-starter-v17.2.0 (1)/src/app/modules/admin/apps/dash-board/services/services.component.ts
+++ fuse-starter-v17.2.0 (1)/src/app/modules/admin/apps/dash-board/services/services.component.ts
@@ -25,17 +25,17 @@ export class ServicesComponent {
     {
       title: 'Đại lý vận tải hàng hoá',
       description: 'Expert handling of oversized, overweight, and delicate shipments.',
-      img: 'assets/green-line/Overview/Cont.png'
+      img: 'assets/green-line/Overview/Train.bmp'
     },
     {
       title: 'Giải pháp thân thiện',
       description: 'Secure storage with inventory management and cross-docking at strategic locations.',
-      img: 'assets/green-line/Overview/Cont.png'
+      img: 'assets/green-line/Overview/Train2.bmp'
     },
     {
       title: 'Giảm thiểu khí thải',
       description: 'Safe transport for sensitive goods with advanced refrigeration and monitoring.',
-      img: 'assets/green-line/Overview/Cont.png'
+      img: 'assets/green-line/Overview/Train3.png'
     }
   ];
 }
